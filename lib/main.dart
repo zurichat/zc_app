a3d5fec00cc9f89890f33449267a3b6328b74157
Arch997 <archibongc4@gmail.com>
2021-08-29 10:06:31 +0100

    Removed second MaterialApp instance in HomeView and unchecked debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       navigatorKey: AppNavigator.key,
       onGenerateRoute: AppRouter.generateRoute,
       title: 'ZuriChat App',
