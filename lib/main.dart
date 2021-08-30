ce79eaf9e105a4df2a477802ab2225e1d35ee275
Ademola Fadumo <demolafadumo@gmail.com>
2021-08-30 03:12:37 +0100

    responsive design tools and ui helpers

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app_routing/app_navigator.dart';
 import 'app_routing/app_router.dart';
 import 'views/home/home_view.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(),
+      ),
+  );
+}
 
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
---
+++ lib/utilities/constants/const_ui_helpers.dart
@@ -0,0 +1,21 @@
+// Horizontal Spacing
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+final Widget horizontalSpaceTiny = SizedBox(width: 4.0.w);
+final Widget horizontalSpaceSmall = SizedBox(width: 8.0.w);
+final Widget horizontalSpaceRegular = SizedBox(width: 16.0.w);
+final Widget horizontalSpaceMedium = SizedBox(width: 24.0.w);
+final Widget horizontalSpaceLarge = SizedBox(width: 48.0.w);
+final Widget horizontalSpaceExtraLarge = SizedBox(height: 72.w);
+final Widget horizontalSpaceExtraExtraLarge = SizedBox(height: 96.w,);
+
+// Vertical Spacing
+final Widget verticalSpaceTiny = SizedBox(height: 4.0.h);
+final Widget verticalSpaceSmall = SizedBox(height: 8.0.h);
+final Widget verticalSpaceRegular = SizedBox(height: 16.0.h);
+final Widget verticalSpaceMedium = SizedBox(height: 24.0.h);
+final Widget verticalSpaceExtraMedium = SizedBox(height: 32.0.h);
+final Widget verticalSpaceLarge = SizedBox(height: 48.0.h);
+final Widget verticalSpaceExtraLarge = SizedBox(height: 72.h);
+final Widget verticalSpaceExtraExtraLarge = SizedBox(height: 96.h,);
