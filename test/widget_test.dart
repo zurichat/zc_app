7fa79a48139269c0328445a2a5e6b6a71ee8dfc9
Paul Eke <pauleke65@gmail.com>
2021-08-30 01:29:41 +0100

    final commit, Closes #505

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,5 +14,7 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+
+    //Tests should go here
   });
 }
