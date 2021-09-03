748e5787f3232bdadbe2f77f65b4fa63a12106d4
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-09-03 23:27:10 +0100

    Wrap {@link OnboardingPage} in {@link SafeArea} to avoid overflowing into Status Bar

--- lib/ui/nav_pages/onboarding_page/onboarding_page.dart
+++ lib/ui/nav_pages/onboarding_page/onboarding_page.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hng/app/app.router.dart';
-import 'package:hng/ui/view/home/home_view.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 class OnboardingPage extends StatefulWidget {
@@ -15,28 +15,34 @@ class _OnboardingPageState extends State<OnboardingPage> {
 
   @override
   Widget build(BuildContext context) {
-    return IntroductionScreen(
-      pages: [
-        PageViewModel(
-            title: 'Perfect Collaboration App For Teams',
-            body:
-                'Chat with other team members without any distractions from the world',
-            image: Image.asset('assets/images/onboarding_screen_0.png')),
-        PageViewModel(
-            title: 'Music Room',
-            body:
-                'Now you can listen to your favourite tracks right in the App',
-            image: Image.asset('assets/images/onboarding_screen_1.png')),
-        PageViewModel(
-            title: 'Chess Room',
-            body:
-                'Chat with other team members without any distractions from the world',
-            image: Image.asset('assets/images/onboarding_screen_2.png')),
-      ],
-      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
-      onDone: () => Navigator.of(context).pushReplacementNamed(Routes.homeView),
-      showDoneButton: true,
-      showNextButton: false,
+    return SafeArea(
+      child: IntroductionScreen(
+        key: introKey,
+        globalBackgroundColor: Colors.white,
+        pages: [
+          PageViewModel(
+              title: 'Perfect Collaboration App For Teams',
+              body:
+                  'Chat with other team members without any distractions from the world',
+              image: Image.asset('assets/images/onboarding_screen_0.png')),
+          PageViewModel(
+              title: 'Music Room',
+              body:
+                  'Now you can listen to your favourite tracks right in the App',
+              image: Image.asset('assets/images/onboarding_screen_1.png')),
+          PageViewModel(
+              title: 'Chess Room',
+              body:
+                  'Chat with other team members without any distractions from the world',
+              useScrollView: true,
+              image: Image.asset('assets/images/onboarding_screen_2.png')),
+        ],
+        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+        onDone: () =>
+            Navigator.of(context).pushReplacementNamed(Routes.homeView),
+        showDoneButton: true,
+        showNextButton: false,
+      ),
     );
   }
 }
