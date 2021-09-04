8d3644c19c37c551a402023ae67d2761c2f0b1a7
AKPAKPAN ANIEKAN-ABASI <45481950+aniekan12@users.noreply.github.com>
2021-09-04 16:24:59 +0100

    Update channel_page_viewmodel.dart

--- lib/ui/view/channel/channel_page_viewmodel.dart
+++ lib/ui/view/channel/channel_page_viewmodel.dart
@@ -1,13 +1,3 @@
 import 'package:stacked/stacked.dart';
 
-class ChannelPageViewModel extends IndexTrackingViewModel {
-  static final ChannelPageViewModel instance = ChannelPageViewModel._internal();
-
-  factory ChannelPageViewModel() {
-    return instance;
-  }
-
-  ChannelPageViewModel._internal();
-
-  init() {}
-}
+class ChannelPageViewModel extends BaseViewModel {}
