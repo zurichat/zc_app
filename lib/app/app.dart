ec3ffcb65cec286f98b18169f04cf8067ac54b12
Mariam Hamzat <titilayobolamide247@gmail.com>
2021-09-04 12:24:38 +0100

    Implemented Workspace View and viewmodel
    I added the necessary assets, generated the routes for the workspace view

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:hng/ui/view/nav_bar/nav_bar_view.dart';
 import 'package:hng/ui/view/preference/preference_view.dart';
+import 'package:hng/ui/view/workspace/workspace_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:stacked_themes/stacked_themes.dart';
@@ -11,6 +12,7 @@ import '../ui/view/login/login_view.dart';
     MaterialRoute(page: NavBarView, initial: true),
     MaterialRoute(page: LoginView),
     MaterialRoute(page: PreferenceView),
+    MaterialRoute(page: WorkspaceView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
