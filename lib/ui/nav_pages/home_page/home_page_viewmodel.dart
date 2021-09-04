b13a5b79623685fa9fa21560b34f7695d73f697c
bountyforager <bountyforager@gmail.com>
2021-09-04 18:00:44 +0100

    Revert "Merge branch 'dev' into feature"
    This reverts commit 5043dbd763e7196b1b321733874725bdbb24da29, reversing
    changes made to 87e2d13d48af50fb7aa89a00c5cff33466febd9e.

--- lib/ui/nav_pages/home_page/home_page_viewmodel.dart
+++ lib/ui/nav_pages/home_page/home_page_viewmodel.dart
@@ -3,13 +3,8 @@ import 'package:stacked_services/stacked_services.dart';
 
 import '../../../app/app.router.dart';
 
-//final _navigationService = locator<NavigationService>();
-
 class HomePageViewModel extends BaseViewModel {
   nToPref() {
     NavigationService().navigateTo(Routes.preferenceView);
-  }
-  nToWorkspace(){
-    NavigationService().navigateTo(Routes.workspaceView);
   }
 }
--- lib/ui/shared/styles.dart
+++ lib/ui/shared/styles.dart
@@ -1,77 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'colors.dart';
 
 class AppTextStyles {
-  static TextStyle heading6 = GoogleFonts.lato(
+  static const TextStyle heading6 = TextStyle(
     fontWeight: FontWeight.w700,
     color: AppColors.greyishColor,
     fontSize: 24,
   );
-  static TextStyle heading5 = GoogleFonts.lato(
+  static const TextStyle heading5 = TextStyle(
     fontWeight: FontWeight.w500,
     color: AppColors.greyishColor,
     fontSize: 16,
   );
-  static TextStyle body1Regular = GoogleFonts.lato(
+  static const TextStyle body1Regular = TextStyle(
     fontWeight: FontWeight.w400,
     color: AppColors.greyishColor,
     fontSize: 16,
   );
-  static TextStyle normalText = GoogleFonts.lato(
-    fontWeight: FontWeight.w400,
-    fontSize: 14,
-  );
-  static TextStyle body2Medium = GoogleFonts.lato(
+  static const TextStyle body2Medium = TextStyle(
     fontWeight: FontWeight.w500,
     color: AppColors.greyishColor,
     fontSize: 14,
   );
-  static TextStyle body1Bold = GoogleFonts.lato(
+  static const TextStyle body1Bold = TextStyle(
     fontWeight: FontWeight.w700,
-    color: AppColors.deepBlackColor,
+    color: AppColors.greyishColor,
     fontSize: 16,
   );
-  static TextStyle body2Bold = GoogleFonts.lato(
+  static const TextStyle body2Bold = TextStyle(
     fontWeight: FontWeight.w700,
     color: AppColors.greyishColor,
     fontSize: 14,
   );
-  static TextStyle timestamp = GoogleFonts.lato(
-    fontWeight: FontWeight.w400,
-    color: AppColors.deepBlackColor,
-    fontSize: 12,
-  );
-  static TextStyle unreadText = GoogleFonts.lato(
-    fontWeight: FontWeight.w700,
-    color: AppColors.whiteColor,
-    fontSize: 12,
-  );
-
-  // unread count
-  static Widget unreadCount(int count) {
-    return Container(
-      height: 24,
-      width: 24,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: AppColors.unreadMessageColor,
-      ),
-      child: Center(
-        child: Text(count.toString(), style: AppTextStyles.unreadText),
-      ),
-    );
-  }
 
   // button
-  static TextStyle buttonText = GoogleFonts.lato(
+  static const TextStyle buttonText = TextStyle(
     fontWeight: FontWeight.w500,
     color: AppColors.whiteColor,
     fontSize: 16,
   );
 }
+
+
 
 const logoAsset = AssetImage('assets/logo/hng_logo.png');
 const dummyNetworkImage =
