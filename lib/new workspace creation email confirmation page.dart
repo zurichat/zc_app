9026c2bfc54b07ec4180f0f6e1af23aaf48331f2
kennyobey <kennyobey@gmail.com>
2021-09-04 09:14:12 +0100

    New changes to email button

--- lib/new workspace creation email confirmation page.dart
+++ lib/new workspace creation email confirmation page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'dart:io'; 
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_appavailability/flutter_appavailability.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+ void openEmailApp(BuildContext context){
+     try{
+         AppAvailability.launchApp(Platform.isIOS ? "message://" : "com.google.android.gm").then((_) {
+                 print("App Email launched!");
+               }).catchError((err) {
+                 // ignore: deprecated_member_use
+                 Scaffold.of(context).showSnackBar(SnackBar(
+                     content: Text("App Email not found!")
+                 ));
+                 print(err);
+               });
+     } catch(e) {
+       // ignore: deprecated_member_use
+       Scaffold.of(context).showSnackBar(SnackBar(content: Text("Email App not found!")));
+     }
+ }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,6 +68,7 @@ class MyApp extends StatelessWidget {
                     ),
                     onPressed: () {
                       print('Pressed');
+                      openEmailApp(context);
                     },
                   )
                 ]),
