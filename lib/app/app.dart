1e3e8d48fd35f468f8992dbb483d79004f3ada6a
Blazebrain <davidadegoke16@gmail.com>
2021-09-04 21:21:51 +0100

    update

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,14 @@
-import 'package:hng/ui/view/nav_bar/nav_bar_view.dart';
-import 'package:hng/ui/view/preference/preference_view.dart';
-import 'package:hng/ui/view/workspace/workspace_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:stacked_themes/stacked_themes.dart';
 
-import 'package:hng/services/connectivity_service.dart';
-
+import '../services/connectivity_service.dart';
+import '../services/local_storage_services.dart';
 import '../ui/view/login/login_view.dart';
+import '../ui/view/nav_bar/nav_bar_view.dart';
+import '../ui/view/preference/preference_view.dart';
+import '../ui/view/workspace/workspace_view.dart';
 
 @StackedApp(
   routes: [
@@ -19,6 +20,10 @@ import '../ui/view/login/login_view.dart';
   dependencies: [
     LazySingleton(classType: NavigationService),
     LazySingleton(classType: ThemeService),
+    Presolve(
+      classType: SharedPreferenceLocalStorage,
+      presolveUsing: SharedPreferences.getInstance,
+    ),
     LazySingleton(classType: ConnectivityService)
   ],
 )
--- lib/services/connectivity_service.dart
+++ lib/services/connectivity_service.dart
@@ -2,27 +2,28 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:connectivity/connectivity.dart';
-import 'package:hng/utilities/enums.dart';
 import 'package:stacked/stacked.dart';
 
+import '../utilities/enums.dart';
 
 class ConnectivityService with ReactiveServiceMixin {
   // Creates a single instance by calling the '_internal' constructor.
-  static final ConnectivityService _connectionStatus = ConnectivityService._internal();
+  static final ConnectivityService _connectionStatus =
+      ConnectivityService._internal();
   ConnectivityService._internal();
-  
+
   // Retrieve the instance through the app.
   static ConnectivityService getInstance() => _connectionStatus;
-  
+
   // Flags to keep track of connection state.
   bool hasConnection = false;
   ConnectivityStatus networkStatus = ConnectivityStatus.Offline;
-  
+
   // Allow subscription to connection changes.
   StreamController connectionChangeController = StreamController.broadcast();
 
   final Connectivity _connectivity = Connectivity();
-  
+
   // Listen for changes on initialization.
   void initialize() {
     _connectivity.onConnectivityChanged.listen(_connectionChange);
@@ -34,7 +35,7 @@ class ConnectivityService with ReactiveServiceMixin {
   void _connectionChange(ConnectivityResult result) {
     checkConnection();
   }
-  
+
   // Function that does the actual connection tests.
   Future<bool> checkConnection() async {
     ConnectivityStatus prevStatus = ConnectivityStatus.Offline;
@@ -48,11 +49,11 @@ class ConnectivityService with ReactiveServiceMixin {
         hasConnection = false;
         networkStatus = ConnectivityStatus.Offline;
       }
-    } on SocketException catch(_) {
+    } on SocketException catch (_) {
       hasConnection = false;
       networkStatus = ConnectivityStatus.Offline;
     }
-    
+
     // Status change sends out an update to all listeners.
     if (prevStatus != networkStatus) {
       connectionChangeController.add(networkStatus);
@@ -61,12 +62,12 @@ class ConnectivityService with ReactiveServiceMixin {
 
     return hasConnection;
   }
-  
+
   // Helper method to listen to continuous changes after initialization
   void onChange() {
     _connectivity.onConnectivityChanged.listen(_connectionChange);
   }
-  
+
   void dispose() {
     connectionChangeController.close();
   }
