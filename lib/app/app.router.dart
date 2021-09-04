aa5fab7f5d0ced83988a1db71f7ebbf4c422eef4
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-09-04 20:45:57 +0100

    Run "flutter pub run build_runner build --delete-conflicting-outputs" to generate routes

--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -7,22 +7,24 @@
 // ignore_for_file: public_member_api_docs
 
 import 'package:flutter/material.dart';
-import 'package:hng/ui/nav_pages/onboarding_page/onboarding_page.dart';
 import 'package:stacked/stacked.dart';
 
 import '../ui/view/login/login_view.dart';
 import '../ui/view/nav_bar/nav_bar_view.dart';
+import '../ui/view/onboarding/onboading_view.dart';
 import '../ui/view/preference/preference_view.dart';
 import '../ui/view/workspace/workspace_view.dart';
 
 class Routes {
-  static const String navBarView = '/';
   static const String loginView = '/login-view';
+  static const String navBarView = '/nav-bar-view';
+  static const String onboardingView = '/';
   static const String preferenceView = '/preference-view';
   static const String workspaceView = '/workspace-view';
   static const all = <String>{
-    navBarView,
     loginView,
+    navBarView,
+    onboardingView,
     preferenceView,
     workspaceView,
   };
@@ -32,23 +34,30 @@ class StackedRouter extends RouterBase {
   @override
   List<RouteDef> get routes => _routes;
   final _routes = <RouteDef>[
-    RouteDef(Routes.navBarView, page: NavBarView),
     RouteDef(Routes.loginView, page: LoginView),
+    RouteDef(Routes.navBarView, page: NavBarView),
+    RouteDef(Routes.onboardingView, page: OnboardingView),
     RouteDef(Routes.preferenceView, page: PreferenceView),
     RouteDef(Routes.workspaceView, page: WorkspaceView),
   ];
   @override
   Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
   final _pagesMap = <Type, StackedRouteFactory>{
+    LoginView: (data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => const LoginView(),
+        settings: data,
+      );
+    },
     NavBarView: (data) {
       return MaterialPageRoute<dynamic>(
         builder: (context) => const NavBarView(),
         settings: data,
       );
     },
-    LoginView: (data) {
+    OnboardingView: (data) {
       return MaterialPageRoute<dynamic>(
-        builder: (context) => const LoginView(),
+        builder: (context) => const OnboardingView(),
         settings: data,
       );
     },
