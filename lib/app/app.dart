40e38fe141bee45d976ad44fcaf111dd61054136
Destan <egbulonudestiny@gmail.com>
2021-09-04 21:33:02 +0100

    File Update

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import 'package:hng/ui/view/channel_search/channel_search_view.dart';
-=======
 import 'package:shared_preferences/shared_preferences.dart';
->>>>>>> 2ebaedcc3263ad6f1988d13a09146a5e9d25dbef
 import 'package:hng/ui/view/nav_bar/nav_bar_view.dart';
 import 'package:hng/ui/view/onboarding/onboading_view.dart';
 import 'package:hng/ui/view/preference/preference_view.dart';
@@ -22,7 +18,6 @@ import '../ui/view/login/login_view.dart';
     MaterialRoute(page: OnboardingView, initial: true),
     MaterialRoute(page: PreferenceView),
     MaterialRoute(page: WorkspaceView),
-    MaterialRoute(page: ChannelSearchPageView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
