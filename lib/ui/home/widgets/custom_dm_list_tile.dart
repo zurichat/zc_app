48e87346975103bcce5d70522a39b34641d57adb
Hassan Teslim <hassanteslim12@gmail.com>
2021-09-04 10:59:51 +0100

    Dm chat screen

--- lib/ui/home/widgets/custom_dm_list_tile.dart
+++ lib/ui/home/widgets/custom_dm_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hng/ui/direct_message/direct_message.dart';
 
 import '../../../utilities/constants/styles.dart';
 
@@ -13,22 +14,28 @@ class CustomDMListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CircleAvatar(
-          backgroundImage: AssetImage(imagelink),
-          radius: 20.0,
-        ),
-        const SizedBox(width: 8),
-        Text(
-          userName,
-          style: const TextStyle(
-            fontWeight: FontWeight.w700,
-            color: greyishColor,
-            fontSize: 15,
+    return InkWell(
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => DirectMessage(username: userName))),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage(imagelink),
+            radius: 20.0,
           ),
-        ),
-      ],
+          const SizedBox(width: 8),
+          Text(
+            userName,
+            style: const TextStyle(
+              fontWeight: FontWeight.w700,
+              color: greyishColor,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
