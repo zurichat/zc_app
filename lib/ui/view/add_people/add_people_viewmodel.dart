b66d1816ce6f44f348269847cd44d0d026ebea0c
Agbeniga Caleb <agbenigacaleb@gmail.com>
2021-09-05 05:41:00 +0100

    Implemented Add people screen

--- lib/ui/view/add_people/add_people_viewmodel.dart
+++ lib/ui/view/add_people/add_people_viewmodel.dart
@@ -1,7 +1,7 @@
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-import '../../app/app.locator.dart';
+import '../../../app/app.locator.dart';
 
 class AddPeopleViewModel extends BaseViewModel {
   final navigator = locator<NavigationService>();
