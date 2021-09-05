11acbc455aca55daa4bc4b23fe384d1aae49a84e
Blazebrain <davidadegoke16@gmail.com>
2021-09-05 14:51:30 +0100

    update

--- lib/ui/view/otp/otp_viewmodel.dart
+++ lib/ui/view/otp/otp_viewmodel.dart
@@ -1,6 +1,7 @@
-import '../../../app/app.router.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+
+import '../../../app/app.router.dart';
 
 class OtpViewModel extends BaseViewModel {
   navigatorToHome() {
