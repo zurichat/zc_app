b343b1f80db7f231c0bc72ac41a56d370cf1c54b
Godwin <uchegod67@gmail.com>
2021-09-06 15:48:32 +0100

    ViewProfile Draggable

--- lib/ui/view_profile_page2/view_profile.dart
+++ lib/ui/view_profile_page2/view_profile.dart
@@ -1,10 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ViewProfile extends StatelessWidget {
   final bool isActive;
 
-  const ViewProfileP({
+  const ViewProfile({
     Key? key,
     this.isActive = true,
   }) : super(key: key);
@@ -12,89 +14,100 @@ class ViewProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.3,
-                  width: MediaQuery.of(context).size.width,
-                  child: Stack(fit: StackFit.expand, children: [
-                    Image(
-                      fit: BoxFit.cover,
-                      image: AssetImage('assets/profile.jpeg'),
-                    ),
-                  ]),
-                ),
-                Positioned(
-                  bottom: 20.0,
-                  left: 12.0,
-                  child: Row(
-                    children: [
-                      Text(
-                        'Amanda Josiana Kee',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.normal,
-                            fontSize: 18.0),
-                      ),
-                      Container(
-                        //Icon wrapped in container with margin, so long names with stack over/below it
-                        margin: EdgeInsets.all(8.0),
-                        child: Icon(
-                          Icons.circle,
-                          size: 12.0,
-                          color: isActive ? Colors.green : Colors.transparent,
+      child: DraggableScrollableSheet(
+          initialChildSize: 0.9,
+          minChildSize: 0.8,
+          maxChildSize: 1.0,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              controller: scrollController,
+              child: Container(
+                child: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.3,
+                          width: MediaQuery.of(context).size.width,
+                          child: Stack(fit: StackFit.expand, children: [
+                            Image(
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/profile.jpeg'),
+                            ),
+                          ]),
                         ),
+                        Positioned(
+                          bottom: 20.0,
+                          left: 12.0,
+                          child: Row(
+                            children: [
+                              Text(
+                                'Amanda Josiana Kee',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 18.0),
+                              ),
+                              Container(
+                                //Icon wrapped in container with margin, so long names with stack over/below it
+                                margin: EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.circle,
+                                  size: 12.0,
+                                  color: isActive ? Colors.green : Colors.transparent,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 16.0, left: 20.0, right: 16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ProfileAction(title: Text('Message'), onTap: () {}),
+                              ProfileAction(
+                                  title: Text('Edit Profile'), onTap: () {}),
+                              ProfileAction(
+                                  title: Icon(Icons.more_horiz), onTap: () {}),
+                            ],
+                          ),
+                          Divider(),
+                          ProfileList(
+                              title: 'What I do', description: 'Mobile Development'),
+                          Divider(),
+                          ProfileList(
+                              title: 'Display Name', description: 'Amanda Josiana'),
+                          Divider(),
+                          ProfileList(
+                            title: 'Status',
+                            description: '4',
+                          ),
+                          Divider(),
+                          ProfileList(
+                            title: 'Mobile Number',
+                            description: '+23481999444',
+                          ),
+                          Divider(),
+                          ProfileList(
+                            title: 'Email Address',
+                            description: 'praiseajepe@gmail.com',
+                          )
+                        ],
                       ),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 16.0, left: 20.0, right: 16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ProfileAction(title: Text('Message'), onTap: () {}),
-                      ProfileAction(title: Text('Edit Profile'), onTap: () {}),
-                      ProfileAction(
-                          title: Icon(Icons.more_horiz), onTap: () {}),
-                    ],
-                  ),
-                  Divider(),
-                  ProfileList(
-                      title: 'What I do', description: 'Flutter Developer'),
-                  Divider(),
-                  ProfileList(
-                      title: 'Display Name', description: 'Uche Socrates'),
-                  Divider(),
-                  ProfileList(
-                    title: 'Status',
-                    description: '4',
-                  ),
-                  Divider(),
-                  ProfileList(
-                    title: 'Mobile Number',
-                    description: '+23481999444',
-                  ),
-                  Divider(),
-                  ProfileList(
-                    title: 'Email Address',
-                    description: 'uchesocrates@gmail.com',
-                  )
-                ],
               ),
-            )
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
@@ -122,7 +135,7 @@ class ProfileAction extends StatelessWidget {
               border: Border.all(color: Colors.black54)),
           child: Padding(
             padding:
-                const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+            const EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
             child: title,
           )),
     );
