a34801dbb3ba698630b7e335ea77f881818085a3
The-indigo <adepojuadeyemi11@gmail.com>
2021-09-14 09:56:10 +0100

    corrected duplicate import

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
           theme: regularTheme,
           darkTheme: darkTheme,
           themeMode: themeMode,
-          initialRoute: Routes.splashview,
+          initialRoute: Routes.channelPageView,
         ),
       ),
     );
