ee42d68a8e19ffd837b21c45a0d4e6dc1f2f183c
Xclusivecyborg <Xclusivecyborg@gmail.com>
2021-09-16 22:56:23 +0100

    Removed dependencies on Velocity_X and moved navigation into the viewmodel.

--- lib/ui/view/channel/channel_info/widgets/third_section.dart
+++ lib/ui/view/channel/channel_info/widgets/third_section.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hng/ui/view/channel/channel_info/channel_info_view_model.dart';
 import 'package:hng/ui/view/channel/channel_members/channel_members_list.dart';
+import 'package:stacked/stacked.dart';
 
 import '../../../../shared/colors.dart';
 import 'textstyles.dart';
@@ -10,144 +12,144 @@ class ThirdSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      alignment: Alignment.center,
-      margin: const EdgeInsets.only(right: 5, left: 5, bottom: 15),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(2),
-          border: Border.all(width: 1.0, color: AppColors.borderColor)),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 9,
-                  top: 19,
+    return ViewModelBuilder<ChannelInfoViewModel>.reactive(
+      viewModelBuilder: () => ChannelInfoViewModel(),
+      builder: (context, model, child) => Container(
+        width: MediaQuery.of(context).size.width,
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(right: 5, left: 5, bottom: 15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(2),
+            border: Border.all(width: 1.0, color: AppColors.borderColor)),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 9,
+                    top: 19,
+                  ),
+                  child: const Icon(
+                    Icons.group_outlined,
+                    color: AppColors.deepBlackColor,
+                    size: 28,
+                  ),
                 ),
-                child: const Icon(
-                  Icons.group_outlined,
-                  color: AppColors.deepBlackColor,
-                  size: 28,
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 11,
+                    top: 19,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      model.navigatoToMembersList();
+                    },
+                    child: Text(
+                      'Members (5)',
+                      style: descriptionStyle(),
+                    ),
+                  ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 11,
-                  top: 19,
+              ],
+            ),
+            Divider(
+              thickness: 0.5,
+              color: AppColors.deepBlackColor.withOpacity(0.5),
+              indent: 53,
+              endIndent: 33,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 9,
+                    top: 13,
+                  ),
+                  child: const Icon(
+                    Icons.person_add_alt_1_outlined,
+                    color: AppColors.deepBlackColor,
+                    size: 28,
+                  ),
                 ),
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ChannelMembersList()));
-                  },
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 11,
+                    top: 13,
+                  ),
                   child: Text(
-                    'Members (5)',
+                    'Add People',
                     style: descriptionStyle(),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Divider(
-            thickness: 0.5,
-            color: AppColors.deepBlackColor.withOpacity(0.5),
-            indent: 53,
-            endIndent: 33,
-          ),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 9,
-                  top: 13,
+              ],
+            ),
+            Divider(
+              thickness: 0.5,
+              color: AppColors.deepBlackColor.withOpacity(0.5),
+              indent: 53,
+              endIndent: 33,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 9,
+                    top: 13,
+                  ),
+                  child: const Icon(
+                    Icons.dashboard_outlined,
+                    color: AppColors.deepBlackColor,
+                    size: 28,
+                  ),
                 ),
-                child: const Icon(
-                  Icons.person_add_alt_1_outlined,
-                  color: AppColors.deepBlackColor,
-                  size: 28,
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 11,
+                    top: 13,
+                  ),
+                  child: Text(
+                    'Plugins (1)',
+                    style: descriptionStyle(),
+                  ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 11,
-                  top: 13,
+              ],
+            ),
+            Divider(
+              thickness: 0.5,
+              color: AppColors.deepBlackColor.withOpacity(0.5),
+              indent: 53,
+              endIndent: 33,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 9,
+                    top: 13,
+                    bottom: 10,
+                  ),
+                  child: const Icon(
+                    Icons.phone_outlined,
+                    color: AppColors.deepBlackColor,
+                    size: 28,
+                  ),
                 ),
-                child: Text(
-                  'Add People',
-                  style: descriptionStyle(),
+                Container(
+                  margin: const EdgeInsets.only(
+                    left: 11,
+                    top: 19,
+                    bottom: 10,
+                  ),
+                  child: Text(
+                    'Start a call',
+                    style: descriptionStyle(),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Divider(
-            thickness: 0.5,
-            color: AppColors.deepBlackColor.withOpacity(0.5),
-            indent: 53,
-            endIndent: 33,
-          ),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 9,
-                  top: 13,
-                ),
-                child: const Icon(
-                  Icons.dashboard_outlined,
-                  color: AppColors.deepBlackColor,
-                  size: 28,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 11,
-                  top: 13,
-                ),
-                child: Text(
-                  'Plugins (1)',
-                  style: descriptionStyle(),
-                ),
-              ),
-            ],
-          ),
-          Divider(
-            thickness: 0.5,
-            color: AppColors.deepBlackColor.withOpacity(0.5),
-            indent: 53,
-            endIndent: 33,
-          ),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 9,
-                  top: 13,
-                  bottom: 10,
-                ),
-                child: const Icon(
-                  Icons.phone_outlined,
-                  color: AppColors.deepBlackColor,
-                  size: 28,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 11,
-                  top: 19,
-                  bottom: 10,
-                ),
-                child: Text(
-                  'Start a call',
-                  style: descriptionStyle(),
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/view/channel/channel_members/channel_members_list.dart
+++ lib/ui/view/channel/channel_members/channel_members_list.dart
@@ -3,7 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hng/ui/shared/colors.dart';
 import 'package:hng/ui/view/plugins/widgets/custom_plugin_list_tile.dart';
 import 'package:stacked/stacked.dart';
-import 'package:velocity_x/velocity_x.dart';
 import '../shared_widgets.dart';
 import 'channel_members_list_model.dart';
 
@@ -13,46 +12,50 @@ class ChannelMembersList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<ChannelMembersModel>.reactive(
-        viewModelBuilder: () => ChannelMembersModel(),
-        builder: (context, viewModel, child) {
-          return Scaffold(
-            appBar: AppBar(
-              title: '#teamsocrates'
-                  .text
-                  .textStyle(GoogleFonts.lato())
-                  .color(Color(0xFF242424))
-                  .bold
-                  .size(20.0)
-                  .make(),
-              backgroundColor: Colors.white,
-              elevation: 1.5,
-              leading: IconButton(
-                icon: Icon(
-                  Icons.clear,
-                  color: Color(0xFF333333),
-                  // size: 24,
-                ),
-                padding: EdgeInsets.only(left: 18),
-                onPressed: () {},
-                iconSize: 25.0,
+      viewModelBuilder: () => ChannelMembersModel(),
+      builder: (context, viewModel, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              '#teamsocrates',
+              style: GoogleFonts.lato(
+                  color: Color(0xFF242424),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
+            ),
+            backgroundColor: Colors.white,
+            elevation: 1.5,
+            leading: IconButton(
+              icon: Icon(
+                Icons.clear,
+                color: Color(0xFF333333),
+                // size: 24,
               ),
-              actions: [
-                InkWell(
-                  child: 'Edit'
-                      .text
-                      .textStyle(GoogleFonts.lato())
-                      .color(Color(0xFF00B87C))
-                      .size(16.0)
-                      .make(),
+              padding: EdgeInsets.only(left: 18),
+              onPressed: () {},
+              iconSize: 25.0,
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0, 20.0, 25.0, 0.0),
+                child: InkWell(
+                  child: Text(
+                    'Edit',
+                    style: GoogleFonts.lato(
+                        color: Color(0xFF00B87C), fontSize: 16.0),
+                  ),
                   highlightColor: Colors.transparent,
                   splashColor: Colors.transparent,
                   onTap: () {},
-                ).pLTRB(0.0, 20.0, 25.0, 0.0),
-              ],
-            ),
-            body: Column(
-              children: [
-                Card(
+                ),
+              )
+            ],
+          ),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0, 16.0, 25.0, 0),
+                child: Card(
                   shadowColor: Colors.black,
                   elevation: 4,
                   child: SearchField(
@@ -60,8 +63,11 @@ class ChannelMembersList extends StatelessWidget {
                     labelText: 'Search for people',
                     // controller: viewModel.editor,
                   ),
-                ).pLTRB(25.0, 16.0, 25.0, 0),
-                GestureDetector(
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0, 24.0, 16.0, 0),
+                child: GestureDetector(
                   onTap: () {},
                   child: CustomPluginPageListTile(
                     leadingIcon: Icon(
@@ -71,42 +77,53 @@ class ChannelMembersList extends StatelessWidget {
                     text: 'Add people',
                     textColor: AppColors.zuriPrimaryColor,
                   ),
-                ).pLTRB(25.0, 24.0, 16.0, 0),
-                ListView.separated(
+                ),
+              ),
+              Expanded(
+                child: ListView.separated(
                   physics: BouncingScrollPhysics(),
                   padding: EdgeInsets.fromLTRB(20.0, 25.0, 12.0, 25.0),
                   itemCount: viewModel.matchingUsers.length,
-                  separatorBuilder: (context, index) => 24.heightBox,
+                  separatorBuilder: (context, index) => SizedBox(height: 24),
                   itemBuilder: (context, index) => Row(
                     children: [
-                      Row(
-                        children: [
-                          Stack(
-                            alignment: Alignment.topRight,
-                            children: [
-                              Image.asset(
-                                      'assets/images/${viewModel.matchingUsers[index].userimg}')
-                                  .p(4.0),
-                              if (viewModel.matchingUsers[index].online)
-                                CircleAvatar(
-                                  backgroundColor: Color(0xFF00B87C),
-                                  radius: 4.0,
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Stack(
+                              alignment: Alignment.topRight,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Image.asset(
+                                    'assets/images/${viewModel.matchingUsers[index].userimg}',
+                                  ),
                                 ),
-                            ],
-                          ),
-                          20.widthBox,
-                          viewModel.matchingUsers[index].userName.text
-                              .textStyle(GoogleFonts.lato())
-                              .color(Color(0xFF424141))
-                              .make(),
-                        ],
-                      ).expand(),
+                                if (viewModel.matchingUsers[index].online)
+                                  CircleAvatar(
+                                    backgroundColor: Color(0xFF00B87C),
+                                    radius: 4.0,
+                                  ),
+                              ],
+                            ),
+                            SizedBox(width: 20.0),
+                            Text(
+                              viewModel.matchingUsers[index].userName,
+                              style: GoogleFonts.lato(
+                                color: Color(0xFF424141),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
-                ).expand(),
-              ],
-            ),
-          );
-        });
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
