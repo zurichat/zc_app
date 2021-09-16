161a305a6ece2ac42aaa407949382697e2f8ae42
mayor04 <mayorwa4@gmail.com>
2021-09-16 08:31:48 +0100

    created the home page widgets

--- lib/ui/nav_pages/home_page/widgets/home_expanded.dart
+++ lib/ui/nav_pages/home_page/widgets/home_expanded.dart
@@ -1,29 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:hng/ui/nav_pages/home_page/widgets/home_list_items.dart';
+import 'package:hng/ui/shared/colors.dart';
+import 'package:hng/ui/shared/text_styles.dart';
+import 'package:hng/utilities/constants.dart';
+
+import '../home_item_model.dart';
+
+enum HomeListType {
+  unreads,
+  channels,
+  messages,
+}
 
 ///Expanded List allows list to be expanded and contracted
 ///if expand is specified as false then the expanded list
 ///would be fixed with all the children displayed
 ///
 ///For example unread has fixed expanded items
-class ExpandedList extends StatelessWidget {
-  final bool expand;
+class HomeExpandedList extends StatefulWidget {
+  final bool canExpand;
   final String title;
+  final List<HomeItemModel> data;
 
-  const ExpandedList({
+  const HomeExpandedList({
     Key? key,
-    this.expand = true,
+    this.canExpand = true,
     required this.title,
+    required this.data,
   }) : super(key: key);
 
   @override
+  _HomeExpandedListState createState() => _HomeExpandedListState();
+}
+
+class _HomeExpandedListState extends State<HomeExpandedList> {
+  @override
   Widget build(BuildContext context) {
-    return expansionTile();
+    List<HomeItemModel> data = widget.data;
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(zSideMargin, 0, zSideMargin, 0),
+      child: expansionTile(
+        children: List.generate(data.length, (i) {
+          HomeItemModel item = data[i];
+          if (item.type == HomeItemType.channels) {
+            return ChannelTextAndIcon(text: item.name ?? '');
+          } else {
+            return DMTextAndIcon(text: item.name ?? '');
+          }
+        }),
+      ),
+    );
   }
 
-  Widget expansionTile() {
-    return ExpansionTile(
-      title: Container(),
-      
+  Widget expansionTile({required List<Widget> children}) {
+    //Unread messages do cannot expand so no expanded list tile is returned
+    if (widget.canExpand) {
+      return Theme(
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+        child: ExpansionTile(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.title,
+                style: ZuriTextStyle.mediumBold(),
+              ),
+              Icon(
+                Icons.add,
+                size: 19,
+                color: AppColors.greyishColor,
+              ),
+            ],
+          ),
+          children: children,
+          tilePadding: EdgeInsets.zero,
+          initiallyExpanded: true,
+          iconColor: AppColors.greyishColor,
+        ),
+      );
+    }
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          height: 50,
+          alignment: Alignment.centerLeft,
+          child: Text(
+            widget.title,
+            style: ZuriTextStyle.mediumBold(),
+          ),
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: children,
+        ),
+      ],
     );
   }
 }
--- lib/ui/nav_pages/home_page/widgets/home_list_items.dart
+++ lib/ui/nav_pages/home_page/widgets/home_list_items.dart
@@ -1,23 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
-class TextAndIcon extends StatelessWidget {
+import 'package:hng/general_widgets/easy_container.dart';
+import 'package:hng/ui/shared/colors.dart';
+import 'package:hng/ui/shared/text_styles.dart';
+
+class ThreadTextAndIcon extends StatelessWidget {
+  const ThreadTextAndIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _TextAndIcon(
+      text: 'Threads',
+      indicatorCount: 0,
+      icon: SvgPicture.asset("assets/icons/svg_icons/lock1.svg"),
+    );
+  }
+}
+
+///Specify the noTopPad as true for all the first child
+///
+//Expanded tile don't allow sizing so we have to decrease
+//the top pad of the first child to make it look visually ok
+class DMTextAndIcon extends StatelessWidget {
   final String text;
-  final int indicatorCount;
-  final Widget icon;
+  final int? indicatorCount;
+  final bool? noTopPad;
 
-  const TextAndIcon({
+  const DMTextAndIcon({
     Key? key,
     required this.text,
-    required this.indicatorCount,
+    this.indicatorCount,
+    this.noTopPad,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        icon,
-        
-      ],
+    return _TextAndIcon(
+      text: text,
+      indicatorCount: indicatorCount,
+      icon: Container(
+        alignment: Alignment.centerLeft,
+        child: EasyContainer(
+          height: 22,
+          width: 22,
+          radius: 3,
+          color: AppColors.paleGreen,
+        ),
+      ),
+    );
+  }
+}
+
+///Specify the noTopPad as true for all the first child
+///
+//Expanded tile don't allow sizing so we have to decrease
+//the top pad of the first child to make it look visually ok
+class ChannelTextAndIcon extends StatelessWidget {
+  final bool? isLocked;
+  final String text;
+  final int? indicatorCount;
+  final bool? noTopPad;
+
+  const ChannelTextAndIcon({
+    Key? key,
+    this.isLocked,
+    required this.text,
+    this.noTopPad,
+    this.indicatorCount,
+  }) : super(key: key);
+
+  Widget prefixIcon() {
+    if (isLocked == true) {
+      return Container();
+    }
+    return Text(
+      '#',
+      style: ZuriTextStyle.largeNormal(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _TextAndIcon(
+      text: text,
+      indicatorCount: indicatorCount,
+      icon: prefixIcon(),
+    );
+  }
+}
+
+///Specify the noTopPad as true for all the first child
+///
+///Expanded tile don't allow sizing so we have to remove
+///the top pad of the first child to make it look visually ok
+///Shows text and Icon together in a Row
+class _TextAndIcon extends StatelessWidget {
+  final String text;
+  final int? indicatorCount;
+  final Widget icon;
+  final bool? noTopPad;
+
+  const _TextAndIcon({
+    Key? key,
+    required this.text,
+    required this.indicatorCount,
+    required this.icon,
+    this.noTopPad,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //Expanded tile don't allow sizing so we have to decrease
+    //the top pad of the first child to make it look visually ok
+    double topPad = 12;
+    if (noTopPad == true) {
+      topPad = 5;
+    }
+
+    //Todo: make text thickness change based on the unreads and read
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, topPad, 0, 12),
+      child: Row(
+        children: [
+          Container(
+            width: 25,
+            alignment: Alignment.center,
+            child: icon,
+          ),
+          SizedBox(
+            width: 9,
+          ),
+          Text(
+            text,
+            style: ZuriTextStyle.mediumNormal(
+              color: Colors.grey[600],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
