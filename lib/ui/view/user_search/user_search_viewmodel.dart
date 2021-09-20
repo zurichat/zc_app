1449ed4beacb3da339dcb86e9e70396c133f5dc7
Quwaysim <qaasim405@gmail.com>
2021-09-20 23:44:55 +0100

    cleanup imports

--- lib/ui/view/user_search/user_search_viewmodel.dart
+++ lib/ui/view/user_search/user_search_viewmodel.dart
@@ -1,5 +1,3 @@
-import 'package:hng/app/app.router.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
 class UserSearchViewModel extends BaseViewModel {}
