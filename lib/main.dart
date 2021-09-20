7eddc19b7ea297885bb4908b4f7aaf0d556decbe
Paul Eke <pauleke65@gmail.com>
2021-09-20 21:36:07 +0100

    Removed bad bottomsheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hng/ui/shared/bottom_sheets/edit_message_bottom_sheet/setupBottomSheetUi.dart';
 import 'package:hng/ui/shared/setup_bottom_sheet_ui.dart';
 import 'package:hng/ui/shared/setup_dialog_ui.dart';
-import 'package:hng/ui/view/dm_user/dm_user_view.dart';
 import 'package:overlay_support/overlay_support.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:stacked_themes/stacked_themes.dart';
@@ -17,7 +15,6 @@ Future main() async {
   await ThemeManager.initialise();
   await setupLocator();
 
-  setupBottomSheetUi2();
   setupDialogUi();
   setupBottomSheetUi();
   AppSnackBar.setupSnackbarUi();
--- lib/ui/shared/setup_bottom_sheet_ui.dart
+++ lib/ui/shared/setup_bottom_sheet_ui.dart
@@ -3,6 +3,7 @@ import 'package:hng/utilities/enums.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import 'bottom_sheets/custom_user_bottomsheet/custom_user_bottom_sheet_view.dart';
+import 'bottom_sheets/edit_message_bottom_sheet/setupBottomSheetUi.dart';
 import 'bottom_sheets/emoji_picker_bottomsheet/emoji_picker_bottomsheet_view.dart';
 
 void setupBottomSheetUi() {
@@ -12,7 +13,9 @@ void setupBottomSheetUi() {
     BottomSheetType.user: (context, sheetRequest, completer) =>
         CustomUserBottomSheetView(request: sheetRequest, completer: completer),
     BottomSheetType.emojiPicker: (context, sheetRequest, completer) =>
-        EmojiPickerBottomSheetView(request: sheetRequest, completer: completer)
+        EmojiPickerBottomSheetView(request: sheetRequest, completer: completer),
+    BottomSheetType.FloatingBox: (context, sheetRequest, completer) =>
+        FloatingBoxBottomSheet(request: sheetRequest, completer: completer)
   };
 
   bottomSheetService.setCustomSheetBuilders(builders);
