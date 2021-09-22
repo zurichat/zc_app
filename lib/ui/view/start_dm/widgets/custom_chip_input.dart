7fd87a680ad197a8523fba0e5dce8f40ed8a674b
bountyforager <bountyforager@gmail.com>
2021-09-22 01:08:18 +0100

    ran build runner

--- lib/ui/view/start_dm/widgets/custom_chip_input.dart
+++ lib/ui/view/start_dm/widgets/custom_chip_input.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chips_input/flutter_chips_input.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hng/models/start_dm_models.dart';
+
 import 'custom_input_chip.dart';
 import 'package:hng/utilities/utilities.dart';
 
