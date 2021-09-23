5f35e408ad943d9419ed4be3296fa4dc14016dc0
Lord_Chris <maduekechris@gmail.com>
2021-09-23 23:17:59 +0100

    create workspace ui complete

--- lib/ui/view/workspace/create_workspace/create_workspace_viewmodel.dart
+++ lib/ui/view/workspace/create_workspace/create_workspace_viewmodel.dart
@@ -3,22 +3,18 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import '../../../../app/app.locator.dart';
-import '../../../../app/app.router.dart';
-import '../../../../models/workspace_model.dart';
 import '../../../../package/base/server-request/workspace_request/workspace_api_service.dart';
 import '../../../../services/local_storage_services.dart';
-import '../../../../utilities/enums.dart';
-import '../../../../utilities/storage_keys.dart';
 
 class CreateWorkSpaceViewModel extends BaseViewModel {
   final _navigation = locator<NavigationService>();
   final snackbar = locator<SnackbarService>();
   final _storage = locator<SharedPreferenceLocalStorage>();
   final _api = WorkSpaceApiService();
-  final _anotherEmail = 'Use another email address';
   final textController = TextEditingController();
+  bool? _checkBoxVal = false;
 
-  get checkBoxVal => true;
+  final pageController = PageController(initialPage: 2);
 
   // String? get userEmail => _storage.getString(StorageKeys.currentUserEmail);
   // String get anotherEmail => _anotherEmail;
@@ -60,5 +56,16 @@ class CreateWorkSpaceViewModel extends BaseViewModel {
   //   }
   // }
 
-  void onCheckBoxChanged(bool? value) {}
+  void onCheckBoxChanged(bool? value) {
+    _checkBoxVal = value;
+    notifyListeners();
+  }
+
+  bool? get checkBoxVal => _checkBoxVal;
+
+  void onInviteTap() {}
+
+  void next() {
+    pageController.nextPage(duration: Duration(seconds: 1), curve: Curves.ease);
+  }
 }
