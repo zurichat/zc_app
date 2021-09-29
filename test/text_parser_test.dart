b40216be6c69a5981c91859020d7472a726addfe
Prince Emelife <princeemelife@gmail.com>
2021-09-29 02:37:31 +0100

    Unit test for RegEx

---
+++ test/text_parser_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  String sample1 = 'I love debugging @eternity has given 07010269';
+  String sample2 = 'www.goog is not a *real* #test  we test@gmail.com';
+  String sample3 = 'let us test for a keyword';
+  String sample4 = 'facebook.com ';
+  String sample5 = 'keyword k5yk5yk';
+  group('RegEx Tests', () {
+    test("Contains Url", () async {
+      expect(
+          RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+              .hasMatch(sample3),
+          false);
+    });
+    test("Contains '@'mentions", () async {
+      expect(RegExp(r'\B@+([\w]+)\b').hasMatch(sample1), true);
+    });
+    test("Contains hashtag", () async {
+      expect(RegExp(r'keyword').hasMatch(sample5), true);
+    });
+    test("Contains hashtag", () async {
+      expect(RegExp(r'(?<=\*)(.*)(\*)').hasMatch(sample2), true);
+    });
+    test("Contains Url", () async {
+      expect(
+          RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+              .hasMatch(sample4),
+          true);
+    });
+  });
+}
