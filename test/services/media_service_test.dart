710d8bd133760f389a19f9865722360f53aa4026
Paul Eke <pauleke65@gmail.com>
2021-10-01 13:08:44 +0100

    final changes

--- test/services/media_service_test.dart
+++ test/services/media_service_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:mockito/mockito.dart';
+import '../helpers/test_constants.dart';
 import '../helpers/test_helpers.dart';
 
 void main() {
@@ -9,19 +9,21 @@ void main() {
     tearDown(() => unregisterServices());
 
     group('Media upload -', () {
-      
-      test('When called it should upload an image and return image address', () async {
-          final mediaService = getAndRegisterMediaServiceMock();
-          final XFile? image;
-        
+      final mediaService = getAndRegisterMediaServiceMock();
+      test('When called it should upload an image and return image address',
+          () async {
+        mediaService.uploadImage(fileMock);
+
+        verify(mediaService.uploadImage(fileMock));
       });
-  //     test(
-  //         'When called it should pick an image from gallery and upload the image',
-  //         () async {
-  //       final mediaService = getAndRegisterMediaServiceMock();
-  //       mediaService.getImage(fromGallery: true);
-  //       verify(mediaService.getImage(fromGallery: true));
-  //     });
+
+      //     test(
+      //         'When called it should pick an image from gallery and upload the image',
+      //         () async {
+      //       final mediaService = getAndRegisterMediaServiceMock();
+      //       mediaService.getImage(fromGallery: true);
+      //       verify(mediaService.getImage(fromGallery: true));
+      //     });
     });
- });
+  });
 }
