6c109342c2bfde35a727be7c7ac7e2dbd8104828
Prince Emelife <princeemelife@gmail.com>
2021-10-01 14:40:01 +0100

    Fixed test errors

--- test/services/text_parser_test.dart
+++ test/services/text_parser_test.dart
@@ -4,12 +4,12 @@ void main() {
   String sample1 = 'I love debugging @eternity has given 07010269';
   String sample2 = 'www.goog is not a *real* #test  we test@gmail.com';
   String sample3 = 'let us test for a keyword';
-  String sample4 = 'facebook.com ';
+  String sample4 = 'www.facebook.com';
   String sample5 = 'keyword k5yk5yk';
   group('RegEx Tests', () {
     test("Contains Url", () async {
       expect(
-          RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+          RegExp(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')
               .hasMatch(sample3),
           false);
     });
@@ -24,7 +24,7 @@ void main() {
     });
     test("Contains Url", () async {
       expect(
-          RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+          RegExp(r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')
               .hasMatch(sample4),
           true);
     });
