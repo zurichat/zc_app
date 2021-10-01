1204896c987aea67dee941046e18e52a4831426e
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-10-01 04:09:50 +0100

    Add the flutter_typeahead dependency Use the TypeAheadField widget to show a suggestion of channel users as user types '@...'
    Signed-off-by: Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>

--- lib/ui/view/channel/channel_search/channel_search_view.dart
+++ lib/ui/view/channel/channel_search/channel_search_view.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:hng/models/channel_members.dart';
+import 'package:hng/models/user_search_model.dart';
 import 'package:hng/ui/shared/colors.dart';
 import 'package:hng/ui/shared/styles.dart';
+import 'package:hng/ui/view/channel/channel_search/widgets/search_result.dart';
 import 'package:hng/ui/view/channel/channel_search/widgets/search_section.dart';
 import 'package:stacked/stacked.dart';
 
@@ -15,8 +18,11 @@ import 'widgets/search_subsection.dart';
 class ChannelSearchPageView extends StatefulWidget {
   final String channelId;
   final List<ChannelMemberModel> channelMembers;
+  late final List<UserSearch> organisationMembers;
+  var searchResults;
+  late int numberOfFileResults = 0, numberOfMessageResults = 0;
 
-  const ChannelSearchPageView(
+  ChannelSearchPageView(
       {Key? key, required this.channelId, required this.channelMembers})
       : super(key: key);
 
@@ -37,6 +43,9 @@ class _ChannelSearchPageViewState extends State<ChannelSearchPageView>
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<ChannelSearchViewModel>.reactive(
+      onModelReady: (ChannelSearchViewModel model) {
+        model.fetchOrganisationMembers();
+      },
       builder: (context, model, child) => Scaffold(
         backgroundColor: _tabController.index == 0
             ? AppColors.greyBackgroundColor
@@ -57,41 +66,93 @@ class _ChannelSearchPageViewState extends State<ChannelSearchPageView>
                   color: Color(0xFF009B69),
                 ),
                 margin: EdgeInsets.only(bottom: 24),
-                child: TextField(
-                  controller: model.searchController,
-                  cursorColor: Colors.white,
-                  keyboardType: TextInputType.text,
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                    focusedBorder: InputBorder.none,
-                    border: InputBorder.none,
-                    prefixIcon: IconButton(
-                      icon: model.searchController.text.isEmpty
-                          ? const Icon(
-                              Icons.search,
-                              color: const Color(0xFFF4F4F4),
-                            )
-                          : const Icon(
-                              Icons.arrow_back_ios_outlined,
-                              color: const Color(0xFFF4F4F4),
-                            ),
-                      iconSize: 20,
-                      onPressed: () => model.searchController.text.isEmpty
-                          ? null
-                          : model.navigateBack(),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.mic_none_outlined),
-                      onPressed: () {},
-                    ),
-                    hintText: 'Search messages, files and users',
-                    hintStyle: TextStyle(
-                      color: AppColors.greyBackgroundColor,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
+                child: TypeAheadField<UserSearch>(
+                  textFieldConfiguration: TextFieldConfiguration(
+                    controller: model.searchController,
+                    //
+                    cursorColor: Colors.white,
+                    onChanged: (pattern) {
+                      model.searchUser(pattern);
+                    },
+                    onSubmitted: (query) {
+                      widget.searchResults = model.searchContent(
+                        widget.channelId,
+                        '',
+                        {
+                          // 'user_id': '61533895582bef54d04efacd',
+                          'content': '$query',
+                        },
+                      );
+                      setState(() {
+                        widget.numberOfFileResults = model.fileResults.length;
+                        widget.numberOfMessageResults =
+                            model.messageResults.length;
+                      });
+                    },
+                    keyboardType: TextInputType.text,
+                    maxLines: 1,
+                    autofocus: true,
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      focusedBorder: InputBorder.none,
+                      border: InputBorder.none,
+                      prefixIcon: IconButton(
+                        icon: model.searchController.text.isEmpty
+                            ? const Icon(
+                                Icons.search,
+                                color: const Color(0xFFF4F4F4),
+                              )
+                            : const Icon(
+                                Icons.arrow_back_ios_outlined,
+                                color: const Color(0xFFF4F4F4),
+                              ),
+                        iconSize: 20,
+                        onPressed: () => model.searchController.text.isEmpty
+                            ? null
+                            : model.navigateBack(),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          Icons.mic_none_outlined,
+                          color: const Color(0xFFF4F4F4),
+                        ),
+                        iconSize: 20,
+                        onPressed: () {},
+                      ),
+                      hintText: 'Search messages, files and users',
+                      hintStyle: TextStyle(
+                        color: AppColors.greyBackgroundColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ),
-                  style: TextStyle(color: Colors.white),
+                  suggestionsCallback: (pattern) async {
+                    return model.memberSearchResults;
+                  },
+                  itemBuilder: (context, suggestion) {
+                    return SearchResult(
+                      userName: suggestion.userName,
+                      fullName:
+                          '${suggestion.firstName} ${suggestion.lastName}',
+                      profileImage: suggestion.imageUrl,
+                      onlineStatus: true,
+                    );
+                  },
+                  noItemsFoundBuilder: (context) {
+                    return SizedBox();
+                  },
+                  onSuggestionSelected: (suggestion) {
+                    model.parseUserName(
+                        userId: suggestion.id!, userName: suggestion.userName!);
+                    print('${suggestion.userName}');
+                    // Navigator.of(context).push(MaterialPageRoute(
+                    //     builder: (context) {
+                    //
+                    //     }
+                    // ));
+                    print('Hey, you!');
+                  },
                 ),
               ),
             ],
@@ -132,8 +193,66 @@ class _ChannelSearchPageViewState extends State<ChannelSearchPageView>
         body: TabBarView(
           controller: _tabController,
           children: [
-            DefaultSearch(widget: widget, model: model),
-            ZeroSearch(contentName: 'designers', contentType: 'file'),
+            widget.searchResults.toString().isEmpty
+                ? ZeroSearch(
+                    contentName: '${model.searchController.text}',
+                    contentType: 'message')
+                : DefaultSearch(widget: widget, model: model),
+            widget.searchResults.toString().isEmpty
+                ? ZeroSearch(
+                    contentName: '${model.searchController.text}',
+                    contentType: 'file',
+                  )
+                : Visibility(
+                    replacement: DefaultSearch(widget: widget, model: model),
+                    child: Flex(
+                      direction: Axis.vertical,
+                      children: [
+                        Flexible(
+                          fit: FlexFit.loose,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(20),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                        '${widget.numberOfFileResults} results')
+                                  ],
+                                ),
+                              ),
+                              ListView.separated(
+                                  shrinkWrap: true,
+                                  itemBuilder:
+                                      (BuildContext context, int index) {
+                                    print('Builder:');
+                                    return MessageResult(
+                                      channelName:
+                                          model.fileResults[index].userId!,
+                                      imageUrl: '',
+                                      message:
+                                          model.fileResults[index].content!,
+                                      userName: '',
+                                      timeStamp:
+                                          model.fileResults[index].timestamp!,
+                                      date: '',
+                                    );
+                                  },
+                                  separatorBuilder:
+                                      (BuildContext context, int index) =>
+                                          const SizedBox(
+                                            height: 20,
+                                          ),
+                                  itemCount: (model.fileResults.length +
+                                      model.messageResults.length))
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
           ],
         ),
       ),
@@ -171,7 +290,10 @@ class DefaultSearch extends StatelessWidget {
             trailingIcon: null,
             onTap: () {},
           ),
-          SearchSection(title: 'Recent searches'),
+          Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: SearchSection(title: 'Recent searches'),
+          ),
           SearchSubSection(
             leadingIcon: Icons.history_sharp,
             title: 'in:#project-socrates Search in Channel',
@@ -184,7 +306,10 @@ class DefaultSearch extends StatelessWidget {
             trailingIcon: Icons.close,
             onTap: () {},
           ),
-          SearchSection(title: 'Narrow your search'),
+          Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: SearchSection(title: 'Narrow your search'),
+          ),
           SearchSubSection(
             leadingIcon: Icons.add_box_outlined,
             subtitle: 'E.g. #project-socrates',
@@ -229,8 +354,7 @@ class DefaultSearch extends StatelessWidget {
 class ZeroSearch extends StatelessWidget {
   final String contentName, contentType;
 
-  const ZeroSearch(
-      {required this.contentName, required this.contentType, Key? key})
+  ZeroSearch({required this.contentName, required this.contentType, Key? key})
       : super(key: key);
 
   @override
@@ -250,28 +374,84 @@ class ZeroSearch extends StatelessWidget {
           ),
         ),
         TextButton(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(
-                // model.text.isEmpty
-                // //TODO Change to brand colors
-                // ? const Color(0xffBEBEBE) :
-                AppColors.zuriPrimaryColor,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+              // model.text.isEmpty
+              // //TODO Change to brand colors
+              // ? const Color(0xffBEBEBE) :
+              AppColors.zuriPrimaryColor,
+            ),
+          ),
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+            child: Container(
+              alignment: Alignment.center,
+              // height: 16,
+              width: 140,
+              child: Text(
+                'Start new search',
+                style: AppTextStyles.buttonText,
               ),
             ),
-            onPressed: () {},
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
-              child: Container(
-                alignment: Alignment.center,
-                // height: 16,
-                width: 140,
-                child: Text(
-                  'Start new search',
-                  style: AppTextStyles.buttonText,
-                ),
-              ),
-            )),
+          ),
+        ),
       ],
+    );
+  }
+}
+
+class MessageResult extends StatelessWidget {
+  final String channelName, imageUrl, message, userName, timeStamp, date;
+
+  MessageResult(
+      {Key? key,
+      required this.channelName,
+      required this.imageUrl,
+      required this.message,
+      required this.userName,
+      required this.timeStamp,
+      required this.date})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(channelName),
+          ListTile(
+            leading: imageUrl.isEmpty
+                ? Image.asset(
+                    'assets/images/user.png',
+                    fit: BoxFit.cover,
+                  )
+                : Image.network(imageUrl),
+            title: Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    userName,
+                    style:
+                        AppTextStyles.archiveTextStyle2.copyWith(fontSize: 14),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Archiving the channel will remover it from the channel list, and close it from all members.',
+                      style: AppTextStyles.body2_400.copyWith(fontSize: 14),
+                      softWrap: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/ui/view/channel/channel_search/channel_search_view_model.dart
+++ lib/ui/view/channel/channel_search/channel_search_view_model.dart
@@ -1,11 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:hng/app/app.locator.dart';
+import 'package:hng/app/app.logger.dart';
+import 'package:hng/models/channel_members.dart';
+import 'package:hng/models/search_result_model.dart';
+import 'package:hng/models/user_search_model.dart';
+import 'package:hng/package/base/server-request/channels/channels_api_service.dart';
+import 'package:hng/package/base/server-request/organization_request/organization_api_service.dart';
+import 'package:hng/services/connectivity_service.dart';
+import 'package:hng/services/local_storage_services.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class ChannelSearchViewModel extends BaseViewModel {
+  static final storageService = locator<SharedPreferenceLocalStorage>();
   final TextEditingController searchController = TextEditingController();
+  final _connectivityService = locator<ConnectivityService>();
+  final _channelApiService = ChannelsApiService();
+  final _organisationApiService = OrganizationApiService();
+  final _log = getLogger('ChannelSearchViewModel');
   final _navigationService = locator<NavigationService>();
 
+  List<UserSearch> membersInOrg = [];
+  List<UserSearch> memberSearchResults = [];
+
+  List<SearchResultModel> messageResults = [];
+  List<SearchResultModel> fileResults = [];
+
+  List<ChannelMemberModel> usersInChannel = [];
+
+  late String userIdSearched;
+
   void navigateBack() => _navigationService.back();
+
+  // void openResult(String url) => _navigationService.navigateTo(Routes.);
+
+  void onChanged(value) {
+    if (value.isNotEmpty) {
+      // membersInOrg = membersInOrg
+      //     .where((channelUsers) => (channelUsers.name.toString().contains(
+      //           '@${value.toLowerCase()}',
+      //         )))
+      //     .toList();
+      if (value.toString().contains('@', 0)) print(value);
+      notifyListeners();
+    } else {
+      membersInOrg = membersInOrg;
+      notifyListeners();
+    }
+    notifyListeners();
+  }
+
+  void fetchOrganisationMembers() async {
+    List? organisationMembers =
+        await _organisationApiService.fetchOrganisationMembers();
+    _log.i('Organisation Members fetched successfully');
+    print(organisationMembers);
+
+    organisationMembers!.forEach((data) async {
+      String userId = data['_id'];
+      String firstName = data['first_name'], lastName = data['last_name'];
+      String userName = data['user_name'];
+      // online status should be here
+      String profileImage = data['image_url'];
+
+      membersInOrg.add(
+        UserSearch(
+          id: userId,
+          firstName: firstName,
+          lastName: lastName,
+          userName: userName,
+          imageUrl: profileImage,
+        ),
+      );
+    });
+  }
+
+  Future<List<SearchResultModel>> searchContent(
+      String channelId, String userId, Map<String, dynamic> searchQuery) async {
+    final results = await _channelApiService.getItems(channelId, searchQuery);
+    results.forEach((element) {
+      if (element.hasFiles)
+        fileResults.add(element);
+      else
+        messageResults.add(element);
+    });
+    return results;
+  }
+
+  List<UserSearch>? searchUser(String text) {
+    List<UserSearch>? matchingUsers = [];
+
+    if (text.isNotEmpty) {
+      if (text.contains('@')) {
+        String userName = text.split('@')[1].split(' ')[0].trim();
+        print('Username: $userName');
+        userName.toLowerCase();
+
+        matchingUsers = membersInOrg
+            .where((member) =>
+                (member.userName!.toLowerCase().contains(userName) ||
+                    member.firstName!.toLowerCase().contains(userName) ||
+                    member.lastName!.toLowerCase().contains(userName)))
+            .toList();
+        notifyListeners();
+      }
+      print('Match: $matchingUsers');
+    }
+    memberSearchResults = matchingUsers;
+    return matchingUsers;
+  }
+
+  void parseUserName({required String userId, required String userName}) {
+    String userName0 = searchController.text.split('@')[1].split(' ')[0].trim();
+    String parsedUserName = userName;
+    if (userName.contains(userName0))
+      parsedUserName = searchController.text.replaceAll(userName0, userName);
+    // searchController.text =
+    searchController.text.replaceFirst(userName0, userName);
+    notifyListeners();
+    // searchController.text =
+    //     searchController.text.split('@')[1].replaceAll(userName0, theUserName);
+    print('The user: $parsedUserName');
+    userIdSearched = userId;
+  }
 }
