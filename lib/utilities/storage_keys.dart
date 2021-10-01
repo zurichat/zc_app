3e91ec8f1cfe9beb6758f9b473bd0f4bb8e5771a
Faith Adetunji Komolafe <59166014+imfaith-hq@users.noreply.github.com>
2021-10-01 20:12:47 +0100

    Update storage_keys.dart

--- lib/utilities/storage_keys.dart
+++ lib/utilities/storage_keys.dart
@@ -15,4 +15,5 @@ class StorageKeys {
   static const status = 'status';
   static const phoneNum = 'phoneNum';
   static const currentUserImageUrl = 'current_url';
+  static const currentUserDrafts = 'current_drafts';
 }
