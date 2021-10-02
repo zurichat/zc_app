fdfda6b41d1b52f507dcab300463bcaa7ba66f2f
Jideije Emeka <jideije.emeka@gmail.com>
2021-10-02 06:13:33 -0700

    unit test for the connectivity service

--- test/services/connectivity_service_test.dart
+++ test/services/connectivity_service_test.dart
@@ -1,5 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../helpers/test_helpers.dart';
 
 void main() {
-  group('ConnectivityServiceTest -', () {});
+  group('ConnectivityServiceTest -', () {
+    setUp(() => registerServices());
+    tearDown(() => unregisterServices());
+
+    group('Connection Test', () {
+      test(' if connection is online', () async {
+        var connectivity = getAndRegisterConnectivityServiceMock();
+        connectivity.initialize();
+        await connectivity.checkConnection().whenComplete(() => true);
+        verify(connectivity.checkConnection().whenComplete(() => true));
+      });
+
+      test(' if connection is offline', () async {
+        var connectivity = getAndRegisterConnectivityServiceMock();
+        connectivity.initialize();
+        await connectivity.checkConnection().whenComplete(() => false);
+        verify(connectivity.checkConnection().whenComplete(() => false));
+      });
+    });
+  });
 }
