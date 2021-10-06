0f9bec201baaa1557a530cf68001da7d71deb09a
Prince Emelife <princeemelife@gmail.com>
2021-10-07 00:44:22 +0100

    Removed unused codes

--- lib/ui/view/dm_user/dm_user_viewmodel.dart
+++ lib/ui/view/dm_user/dm_user_viewmodel.dart
@@ -109,10 +109,7 @@ class DmUserViewModel extends FormViewModel {
             time: DateTime.now(),
           ),
         );
-        // ignore: todo
-        //TODO - fix autoclear
 
-        //clearText();
         notifyListeners();
       }
     });
