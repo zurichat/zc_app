65281afdab8091746dbe92118c201bbdcfbe7a27
mayor04 <mayorwa4@gmail.com>
2021-10-09 11:03:35 +0100

    removed comment

--- lib/ui/nav_pages/home_page/home_page_viewmodel.dart
+++ lib/ui/nav_pages/home_page/home_page_viewmodel.dart
@@ -255,7 +255,6 @@ class HomePageViewModel extends StreamViewModel {
   void onJumpToScreen() {
     navigation.navigateWithTransition(DmJumpToView(),
         transition: NavigationTransition.DownToUp);
-    // navigationService.navigateTo(Routes.dmJumpToView);
   }
 
   @override
