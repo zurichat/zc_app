60184d786d1859b01fb23e63c8474564c54a9065
Hassan Teslim <hassanteslim12@gmail.com>
2021-10-10 01:16:16 +0100

    Merged upstream

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hng/services/notification_service.dart';
 import 'package:hng/ui/shared/setup_bottom_sheet_ui.dart';
 import 'package:hng/ui/shared/setup_dialog_ui.dart';
