4bf0ba6aaa3073609adcdb20b36c67669d2de0a4
Starhkz <67955282+Starhkz@users.noreply.github.com>
2021-10-12 09:22:44 -0700

    Update create_organization.dart

--- lib/ui/view/organization/create_organization/create_organization.dart
+++ lib/ui/view/organization/create_organization/create_organization.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hng/ui/shared/colors.dart';
 
 import 'package:hng/ui/shared/zuri_loader.dart';
 import 'package:hng/ui/shared/zuri_appbar.dart';
