05f86b4ca7c53c532fb56b6be13c6618afd88457
abdussamadbsg <abdulsamadarowosaiye@yahoo.com>
2021-10-12 01:38:11 +0100

    fix

--- lib/ui/shared/bottom_sheets/thread_options_bottomsheet/thread_option_bottomsheet_viewmodel.dart
+++ lib/ui/shared/bottom_sheets/thread_options_bottomsheet/thread_option_bottomsheet_viewmodel.dart
@@ -1,14 +1,12 @@
 import 'package:hng/app/app.locator.dart';
 import 'package:hng/app/app.router.dart';
 import 'package:hng/models/app_model.dart';
-import 'package:hng/package/base/server-request/channels/channels_api_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class ThreadOptionsBottomSheetViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
-  final _channelsApiService = locator<ChannelsApiService>();
   String channelID = '';
 
   List<UserPost>? channelUserMessages = [];
