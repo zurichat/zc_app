e9afc0ff40f0f1c07cd6b5ea754f2f6e589cd5d1
Paul Eke <pauleke65@gmail.com>
2021-10-13 18:24:35 +0100

    removed commented codes

--- lib/ui/nav_pages/dm_page/dm_page_viewmodel.dart
+++ lib/ui/nav_pages/dm_page/dm_page_viewmodel.dart
@@ -6,33 +6,10 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class DmPageViewModel extends FutureViewModel<bool> {
-  //TODO: These were commented out because the backend is currently very unstable, and a lot things are still not working there
   final navigationService = locator<NavigationService>();
-  //final _dmsApiService = locator<DMApiService>();
   final storageService = locator<SharedPreferenceLocalStorage>();
 
   Future<bool> getActiveDMs() async {
-    // final res = await _dmsApiService.getActiveDms();
-    // // final usersInOrg = await _dmsApiService.getAllUsersInOrg();
-    // // storageService.setString(
-    // //     StorageKeys.allMembersinOrganization, jsonEncode(usersInOrg?.data));
-    // // usersInOrg?.data.forEach((e) {
-    // //   if (e['email'] ==
-    // //       storageService.getString(StorageKeys.currentUserEmail)) {
-    // //     storageService.setString(StorageKeys.currentMemberId, e['_id']);
-    // //   }
-    // // });
-    // if (res?.statusCode != 204) {
-    //   List response = res.data;
-    //   if (response.isEmpty) {
-    //     return false;
-    //   } else {
-    //     return true;
-    //   }
-    // } else {
-    //   return false;
-    // }
-
     await Future.delayed(const Duration(seconds: 1));
     return false;
   }
