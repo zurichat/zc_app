4515f70da9307fd6ce945dc3af32cfbfb6c8e0f5
Stanley Ibe <ugoostanleyibe@gmail.com>
2021-10-14 20:58:31 +0100

    Highlight Pinned Messages In ChannelPageView (#949)
    * Highlight Pinned Message In ChannelPageView
    
    * Make Requested Changes
    
    * Make Final Requested Change
    
    * Use Proper AppTextStyle Values
    
    * Remove Unused Import

--- lib/ui/view/channel/channel_view/channel_page_viewmodel.dart
+++ lib/ui/view/channel/channel_view/channel_page_viewmodel.dart
@@ -172,6 +172,7 @@ class ChannelPageViewModel extends FormViewModel {
     String? userId = storage.getString(StorageKeys.currentUserId);
     String? orgId = storage.getString(StorageKeys.currentOrgId);
     String? token = storage.getString(StorageKeys.currentSessionToken);
+    storage.setString(StorageKeys.currentChannelId, channelId);
     // await _channelsApiService.joinChannel(channelId);
     try {
       final res = await _api
@@ -222,20 +223,19 @@ class ChannelPageViewModel extends FormViewModel {
 
       channelUserMessages?.add(
         UserPost(
-          id: data['_id'],
-          displayName: userid,
-          statusIcon: '7️⃣',
-          moment: Moment.now().from(DateTime.parse(data['timestamp'])),
-          message: data['content'],
-          channelType: ChannelType.public,
-          postEmojis: <PostEmojis>[],
-          userThreadPosts: <UserThreadPost>[],
-          channelName: channelId,
-          userImage: 'assets/images/chimamanda.png',
-          userId: userid,
-          channelId: channelId,
-          pinned: data['pinned'],
-        ),
+            id: data['_id'],
+            displayName: userid,
+            statusIcon: '⭐',
+            moment: Moment.now().from(DateTime.parse(data['timestamp'])),
+            message: data['content'],
+            channelType: ChannelType.public,
+            postEmojis: <PostEmojis>[],
+            userThreadPosts: <UserThreadPost>[],
+            channelName: channelId,
+            userImage: 'assets/images/chimamanda.png',
+            userId: userid,
+            channelId: channelId,
+            pinned: data['pinned']),
       );
     });
     isLoading = false;
