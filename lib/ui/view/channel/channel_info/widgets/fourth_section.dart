7159021b17e2093a54c9e61277dc6f84a48957c6
Olakunle Olanrewaju <Olanrewajuolakunle10@gmail.com>
2021-10-14 21:44:14 +0100

    Conflict Resolved

--- lib/ui/view/channel/channel_info/widgets/fourth_section.dart
+++ lib/ui/view/channel/channel_info/widgets/fourth_section.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:hng/constants/app_strings.dart';
-import 'package:hng/ui/shared/styles.dart';
 import 'package:hng/ui/view/channel/channel_info/channel_info_view_model.dart';
-import '../../../../shared/colors.dart';
+import 'new_row_tile.dart';
 
 class FourthSection extends StatelessWidget {
   const FourthSection(this.model, {Key? key}) : super(key: key);
@@ -25,24 +23,8 @@ class FourthSection extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(6.r),
             ),
-            child: Row(
-              children: [
-                SvgPicture.asset(
-                  pinned_message,
-                  color: AppColors.darkGreyColor,
-                  width: 18,
-                  height: 18,
-                ),
-                SizedBox(width: 24.23.w),
-                Text(
-                  PinnedMessages,
-                  style: AppTextStyles.namesStyle.copyWith(
-                    fontSize: 14.sp,
-                    color: Theme.of(context).textTheme.bodyText1!.color,
-                  ),
-                ),
-              ],
-            )),
+            child: const NewRowTile(
+                icon: Icons.push_pin_outlined, text: PinnedMessages)),
       ),
     );
   }
