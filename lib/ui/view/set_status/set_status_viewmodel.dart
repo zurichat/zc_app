48295c1b3ba81dd2f5368d0521592ed02454d969
Qaasim Muhammad Akorede <qaasim405@gmail.com>
2021-10-17 21:33:50 +0100

    Status update fixes (#1000)
    * dnd done
    
    * draft done
    
    * edit profile done
    
    * file search done
    
    * lang and region done
    
    * notifs done
    
    * organisation screens string extracted
    
    * Splash, Profile strings extracted
    
    * feat: threads and user search strings extracted
    
    * feat: flutter analyze issues resolved
    
    * fixed undefined symbol
    
    * stashing work
    
    * removed unused import
    
    * Themes impl: removing static colors
    
    * added theme storage keys
    
    * feat: sign up done
    
    * theme: auth screens
    
    * forgotPass, org and prefDialog
    
    * fixed flutter analyze issue
    
    * preference_view dark mode fixed
    
    * nav_bar dark mode fixed
    
    * org dark mode fixed
    
    * dm dark mode fixed
    
    * home dark mode fixed
    
    * sign out bottomSheet dark mode fixed
    
    * you page dark mode fixed
    
    * added TODO- app crashing bug pulled from upstream
    
    * stashing work - threads
    
    * resolved merge conflicts
    
    * preferences view darkmode impl
    
    * org screens darkmode impld
    
    * threads darkmode impld
    
    * plugin page darkmode impld
    
    * channel darkthem impld
    
    * dm darkmode impld
    
    * added theme unit test
    
    * resolved conflicts
    
    * removed unused imports
    
    * removed test class override
    
    * dark mode UI fix
    
    * removed unused imports
    
    * custom status buttons working
    
    * former status - working
    
    * clear after fixes -start
    
    * expiry value fixed
    
    * you_page - status bug fixed

--- lib/ui/view/set_status/set_status_viewmodel.dart
+++ lib/ui/view/set_status/set_status_viewmodel.dart
@@ -18,24 +18,35 @@ class SetStatusViewModel extends ReactiveViewModel {
   final _snackbarService = locator<SnackbarService>();
   final _statusService = locator<StatusService>();
   final _bottomSheetService = locator<BottomSheetService>();
+
   final log = getLogger('SetStatusViewModel');
-  final String hintText = SetAStatus;
-  bool isLoading = false;
+  String _hintText = SetAStatus;
   String? _tagIcon;
   String _statusText = '';
+  String _expiryTime = 'dont_clear';
+  String get hintText => _hintText;
   String? get tagIcon => _tagIcon;
   String get statusText => _statusText;
+  String get expiryTime => _expiryTime;
+
+  String? _formerStatusText;
+  String? _formerStatusIcon;
+  String? _formerStatusDuration;
+  String? get formerStatusText => _formerStatusText;
+  String? get formerStatusIcon => _formerStatusIcon;
+  String? get formerStatusDuration => _formerStatusDuration;
 
   @override
   List<ReactiveServiceMixin> get reactiveServices => [_statusService];
 
-  loading(status) {
-    isLoading = status;
+  statusValueText(value) {
+    _statusText = value;
     notifyListeners();
   }
 
-  statusValueText(value) {
-    _statusText = value;
+  statusValueIcon(value) {
+    _tagIcon = value;
+    notifyListeners();
   }
 
   void exitPage() {
@@ -47,15 +58,15 @@ class SetStatusViewModel extends ReactiveViewModel {
   }
 
   saveStatus() async {
-    loading(true);
     final orgId = _storageService.getString(StorageKeys.currentOrgId);
     final memberId = _storageService.getString(StorageKeys.idInOrganization);
     final token = _storageService.getString(StorageKeys.currentSessionToken);
     final endpoint = 'organizations/$orgId/members/$memberId/status';
+
     final data = {
       'tag': _tagIcon,
       'text': _statusText,
-      'expiry_time': 'dont_clear'
+      'expiry_time': _expiryTime
     };
     try {
       final response = await _zuriApi.patch('$coreBaseUrl$endpoint',
@@ -63,15 +74,22 @@ class SetStatusViewModel extends ReactiveViewModel {
 
       if (response != null && response.statusCode == 200) {
         _storageService.setString(StorageKeys.statusText, _statusText);
+        _storageService.setString(StorageKeys.statusExpiry, _expiryTime);
         if (tagIcon != null) {
-          _storageService.setString('status_tag_icon', _tagIcon!);
+          _storageService.setString(StorageKeys.statusTagIcon, _tagIcon!);
         }
         _statusService.updateStatusText(_statusText);
+        _statusService.updateStatusIcon(_tagIcon);
+        notifyListeners();
+      } else {
+        _snackbarService.showCustomSnackBar(
+          message: errorOccurred,
+          variant: SnackbarType.failure,
+          duration: const Duration(seconds: 2),
+        );
       }
-      loading(false);
       exitPage();
     } catch (e) {
-      loading(false);
       _snackbarService.showCustomSnackBar(
         message: e.toString(),
         variant: SnackbarType.failure,
@@ -95,5 +113,80 @@ class SetStatusViewModel extends ReactiveViewModel {
     }
   }
 
-  clear() {}
+  init() {
+    _formerStatusText = _storageService.getString(StorageKeys.statusText);
+    _formerStatusIcon = _storageService.getString(StorageKeys.statusTagIcon);
+    _hintText = _formerStatusText ?? SetAStatus;
+    _tagIcon = _formerStatusIcon;
+    _formerStatusDuration =
+        decodeExpiry(_storageService.getString(StorageKeys.statusExpiry));
+  }
+
+  /// Preset Expiry Options (_String_) : dont_clear, thirty_mins, one_hour, four_hours, today, this_week.
+  ///
+  /// Custom Time Format "2006-01-02T15:04:05Z07:00"
+  String? decodeExpiry(String? expiry) {
+    if (expiry == 'dont_clear') {
+      return 'Don\'t clear';
+    } else if (expiry == 'this_week') {
+      return 'this week';
+    } else if (expiry == 'four_hours') {
+      return '4 hours';
+    } else if (expiry == 'one_hour') {
+      return '1 hour';
+    } else if (expiry == 'thirty_mins') {
+      return '30 mins';
+    } else {
+      return expiry;
+    }
+  }
+
+  //Custom Status buttons
+  inMeeting() {
+    _statusText = InMeeting;
+    _hintText = InMeeting;
+    _tagIcon = '📆';
+    _expiryTime = 'one_hour';
+    notifyListeners();
+  }
+
+  commuting() {
+    _statusText = Commuting;
+    _hintText = Commuting;
+    _tagIcon = '🚊';
+    _expiryTime = 'thirty_mins';
+    notifyListeners();
+  }
+
+  offSick() {
+    _statusText = OffSick;
+    _hintText = OffSick;
+    _tagIcon = '🤒';
+    _expiryTime = 'today';
+    notifyListeners();
+  }
+
+  onHoliday() {
+    _statusText = OnHoliday;
+    _hintText = OnHoliday;
+    _tagIcon = '✈';
+    _expiryTime = 'dont_clear';
+    notifyListeners();
+  }
+
+  workingRemotely() {
+    _statusText = WorkingRemotely;
+    _hintText = WorkingRemotely;
+    _tagIcon = '🏡';
+    _expiryTime = 'today';
+    notifyListeners();
+  }
+
+  clear() {
+    _statusText = SetAStatus;
+    _hintText = SetAStatus;
+    _tagIcon = null;
+    _expiryTime = 'dont_clear';
+    notifyListeners();
+  }
 }
